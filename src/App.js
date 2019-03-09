d7742ef6eef66cb547a866589ea4f9e8f6209d64
James Grubb <jamesgrubb@mac.com>
2019-03-09 12:44:31 +0000

    updated map styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
-import { API_KEY } from './utils/key';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-
+import React, { Component } from "react";
+import { API_KEY } from "./utils/key";
+import { Map, GoogleApiWrapper } from "google-maps-react";
+import { mapStyles } from "./theme/mapStyles";
 class App extends Component {
-
   mapClicked(mapProps, map, clickEvent) {
     console.log("mapProps", mapProps);
     console.log("map", map);
@@ -13,17 +12,23 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App" style={{ height: '100vh', width: '100%' }}>
-        <Map google={this.props.google} zoom={6} initialCenter={{
+      <div className="App" style={{ height: "100vh", width: "100%" }}>
+        <Map
+          zoomControl={false}
+          streetViewControl={false}
+          mapTypeControl={false}
+          google={this.props.google}
+          zoom={6}
+          initialCenter={{
             lat: 53.8008,
             lng: -1.54
           }}
           onClick={this.mapClicked}
-        >
-        </Map>
+          styles={mapStyles}
+        />
       </div>
     );
   }
 }
 
-export default GoogleApiWrapper({apiKey: API_KEY})(App);
+export default GoogleApiWrapper({ apiKey: API_KEY })(App);
---
+++ src/theme/colors.js
@@ -0,0 +1,6 @@
+export const colours = {
+  bg: "#B4EDD2",
+  color: "#FFFFFF",
+  primary: "#319177",
+  neutral: "#84897D"
+};
