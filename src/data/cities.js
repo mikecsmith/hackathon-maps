411493de9661cc6599b7c9e95e5fb2f078f70c0b
James Grubb <jamesgrubb@mac.com>
2019-03-09 11:23:43 +0000

    added data

---
+++ src/data/cities.js
@@ -0,0 +1,22 @@
+const cities = [
+  {
+    city: "London",
+    lat: 51.507351,
+    lng: -0.127758
+  },
+  {
+    city: "Brighton",
+    lat: 50.819519,
+    lng: -0.13642
+  },
+  {
+    city: "Southampton",
+    lat: 50.90497,
+    lng: -1.40323
+  },
+  {
+    city: "Cardif",
+    lat: 51.481312,
+    lng: -1.40323
+  }
+];
