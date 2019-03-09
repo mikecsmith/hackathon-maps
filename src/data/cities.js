fa20972b95e2f1b8524d52abac4b5460b3f80735
James Grubb <jamesgrubb@mac.com>
2019-03-09 11:45:45 +0000

    Updated cities

--- src/data/cities.js
+++ src/data/cities.js
@@ -18,5 +18,30 @@ const cities = [
     city: "Cardif",
     lat: 51.481312,
     lng: -1.40323
+  },
+  {
+    city: "Norwich",
+    lat: 52.6281,
+    lng: 1.29935
+  },
+  {
+    city: "Chester",
+    lat: 53.19146,
+    lng: -2.89501
+  },
+  {
+    city: "Nottingham",
+    lat: 52.95512,
+    lng: -1.14917
+  },
+  {
+    city: "Hull",
+    lat: 53.744339,
+    lng: -0.33244
+  },
+  {
+    city: "Grimsby",
+    lat: 53.56544,
+    lng: -0.07519
   }
 ];
